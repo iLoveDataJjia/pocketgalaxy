ecd523f59e2c1faa9a490a116377ed07927ef7f6
iLoveDataJjia <ilovedata.jjia@gmail.com>
2023-12-06 07:49:07 +0100

    WIP: Connector carousel (scrolling effect)

--- frontend/src/components/atoms/StrokedText.tsx
+++ frontend/src/components/atoms/StrokedText.tsx
@@ -5,12 +5,14 @@ interface Props {
 
 export function StrokedText({ text, className }: Props) {
   return (
-    <span className={"relative" + (className ? ` ${className}` : "")}>
-      <span className="absolute inset-0 flex items-center justify-center">
-        {text}
-      </span>
-      <span className="select-none" style={{ WebkitTextStroke: "3px black" }}>
-        {text}
+    <span className={className ? ` ${className}` : ""}>
+      <span className="relative">
+        <span className="absolute inset-0 flex items-center justify-center">
+          {text}
+        </span>
+        <span className="select-none" style={{ WebkitTextStroke: "3px black" }}>
+          {text}
+        </span>
       </span>
     </span>
   );
--- frontend/src/components/organisms/ConnectorCarousel.tsx
+++ frontend/src/components/organisms/ConnectorCarousel.tsx
@@ -1,40 +1,82 @@
+import { useRef, useState } from "react";
 import { StrokedText } from "../atoms/StrokedText";
 import { ConnectorButton } from "../molecules/ConnectorButton";
 
 export function ConnectorCarousel() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isAtStart, setIsAtStart] = useState<boolean>(true);
+  const [isAtEnd, setIsAtEnd] = useState<boolean>(false);
+  const scrollRight = () => {
+    if (ref.current) {
+      ref.current.scrollTo({
+        left: ref.current.scrollLeft + 64,
+      });
+      setIsAtStart(ref.current.scrollLeft === 0);
+      setIsAtEnd(ref.current.scrollLeft == ref.current.scrollWidth);
+    }
+  };
+  const scrollLeft = () => {
+    if (ref.current) {
+      ref.current.scrollTo({
+        left: ref.current.scrollLeft - 64,
+      });
+      setIsAtStart(ref.current.scrollLeft === 0);
+      setIsAtEnd(ref.current.scrollLeft == ref.current.scrollWidth);
+    }
+  };
+
   return (
-    <div className="relative">
-      <StrokedText
-        text="←"
-        className="font-bold text-base absolute md:hidden"
-      />
-      <div className="flex items-center space-x-4 overflow-x-auto scrollbar-hide snap-x">
+    <div className="relative flex justify-between">
+      <button
+        className={
+          "absolute md:hidden translate-y-5 -translate-x-[1.626rem]" +
+          (isAtStart ? " opacity-50" : "")
+        }
+        disabled={isAtStart}
+        onClick={() => scrollLeft()}
+      >
+        <StrokedText text="←" className="font-bold text-base" />
+      </button>
+      <div
+        className="flex items-center space-x-4 overflow-x-auto scrollbar-hide snap-x"
+        ref={ref}
+      >
         <ConnectorButton
           connector="PostgreSQL"
-          className="snap-center"
+          className="snap-start"
           onClick={() => console.log("TODO - PostgreSQL Create")}
         />
         <ConnectorButton
           connector="MySQL"
-          className="snap-center"
+          className="snap-start"
           onClick={() => console.log("TODO - MySQL Create")}
         />
         <ConnectorButton
           connector="MariaDB"
-          className="snap-center"
+          className="snap-start"
           onClick={() => console.log("TODO - MariaDB Create")}
         />
         <ConnectorButton
           connector="MongoDB"
-          className="snap-center"
+          className="snap-start"
           onClick={() => console.log("TODO - MongoDB Create")}
         />
         <ConnectorButton
           connector="MinIO"
-          className="snap-center"
+          className="snap-start"
           onClick={() => console.log("TODO - MinIO Create")}
         />
       </div>
+      <button
+        className={
+          "absolute md:hidden right-0 translate-y-5 translate-x-[1.626rem]" +
+          (isAtEnd ? " opacity-50" : "")
+        }
+        disabled={isAtEnd}
+        onClick={() => scrollRight()}
+      >
+        <StrokedText text="→" className="font-bold text-base" />
+      </button>
     </div>
   );
 }
